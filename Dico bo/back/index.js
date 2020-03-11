b5c1a410777761dbc18102a4e249629c858b2df3
unknown <bilelmek@gmail.com>
2020-03-11 01:24:25 +0100

    init mobile

--- Dico bo/back/index.js
+++ Dico bo/back/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose') ;
 
-const clientRoutes = require('./routes/client') ;
+// const clientRoutes = require('./routes/client') ;
 
 
 const app = express() ;
 
-app.use('/clients' ,clientRoutes);
+// app.use('/clients' ,clientRoutes);
 
 
 mongoose.connect('mongodb://localhost:27017/Dico' , {useNewUrlParser: true} ).then(
