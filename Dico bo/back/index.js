5f6a3349a1c05d42b8d0909726806fca563bd812
unknown <bilelmek@gmail.com>
2020-03-11 01:04:44 +0100

    back office init

---
+++ Dico bo/back/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose') ;
+
+const clientRoutes = require('./routes/client') ;
+
+
+const app = express() ;
+
+app.use('/clients' ,clientRoutes);
+
+
+mongoose.connect('mongodb://localhost:27017/Dico' , {useNewUrlParser: true} ).then(
+    app.listen(3000)
+).catch(
+    err => console.log(err)
+);
